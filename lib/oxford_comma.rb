1de707241b009111720cffb51552f70403df68ad
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-01-30 22:55:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,14 @@
 def oxford_comma(array)
+  case array.length
+  when 1
+    array = array.join("")
+  when 2
+    array[array.length - 1] = "and " + array[array.length - 1]
+    array = array.join(" ")
+  else
+    array[array.length - 1] = "and " + array[array.length - 1]
+    array = array.join(", ")
+  end
   if array.length > 1
     array[array.length - 1] = "and " + array[array.length - 1]
   elsif array.length > 2
