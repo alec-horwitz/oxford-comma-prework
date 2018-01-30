5213c660639af7c5d42070278cea6ec845d81dbc
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-01-30 22:50:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  array[array.length - 1] = "and" + array[array.length - 1]
+  array[array.length - 1] = "and " + array[array.length - 1]
   array = array.join(", ")
   array
 end
