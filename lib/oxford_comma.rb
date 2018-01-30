d10b0aeef4b60b8573f101fb4e3c1593adbec226
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-01-30 22:51:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
-  array[array.length - 1] = "and " + array[array.length - 1]
-  array = array.join(", ")
+  array[array.length - 1] = " and " + array[array.length - 1]
+  if array.length > 2
+    array = array.join(", ")
+  else
+    array = array.join("")
+  end
+  
   array
 end
